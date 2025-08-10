a9459b49e94ab3ac20c695e6031de7994ce27a97
Cumayevomr <cumayevomr8@gmail.com>
2025-08-10 17:16:10 +0400

    search data Load

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let left_btn = document.getElementsByClassName('bi-chevron-left')[0];
 let right_btn = document.getElementsByClassName('bi-chevron-right')[0];
 let cards = document.getElementsByClassName('cards')[0];
+let search = document.getElementsByClassName('search')[0];
 
 left_btn.addEventListener('click', () => {
     cards.scrollLeft -= 140;
@@ -32,12 +33,26 @@ fetch(json_url).then(Response => Response.json())
                 </div>
             `
             cards.appendChild(card);
-
         });
 
-        document.getElementById('title').innerText = date[0].name;
-        document.getElementById('gen').innerText = date[0].name;
-        document.getElementById('date').innerText = date[0].name;
-        document.getElementById('rate').innerHTML = `<span>IMDB</span><i class="bi bi-star-fill"></i> ${date[0].imdb}`;
+        document.getElementById('title').innerText = data[0].name;
+        document.getElementById('gen').innerText = data[0].genre;
+        document.getElementById('date').innerText = data[0].date;
+        document.getElementById('rate').innerHTML = `<span>IMDB</span><i class="bi bi-star-fill"></i> ${data[0].imdb}`;
 
+        // search data Load
+        data.forEach(element => {
+            let {name, imdb, date, sposter, genre, url} = element;
+            let card = document.createElement('a');
+            card.classList.add('card');
+            card.href = url;
+            card.innerHTML = `
+            <img src="${sposter}" alt="">
+            <div class="cont">
+                <h3>${name} </h3>
+                p>${genre}, ${date}, <span>IMDB</span><i class="bi bi-star-fill"></i> ${imdb}</p>
+            </div>
+            `
+            search.appendChild(card);
+        })
     });
