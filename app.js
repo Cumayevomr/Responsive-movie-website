43d2d3104d47465bc0e4bf4d11eb6964ee88aff4
Cumayevomr <cumayevomr8@gmail.com>
2025-08-10 17:05:15 +0400

    date and rate

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ let cards = document.getElementsByClassName('cards')[0];
 left_btn.addEventListener('click', () => {
     cards.scrollLeft -= 140;
 })
-left_btn.addEventListener('click', () => {
+right_btn.addEventListener('click', () => {
     cards.scrollLeft += 140;
 })
 
-let json_url = 'movie.json';
+let json_url = "movie.json";
 
 fetch(json_url).then(Response => Response.json())
     .then((data) => {
         data.forEach((ele, i) => {
             let {name, imdb, date, sposter, bposter, genre, url} = ele;
             let card = document.createElement('a');
-            card.classList.add('add');
+            card.classList.add('card');
             card.href = url;
             card.innerHTML = `
             <img src="${sposter}" alt="${name}" class="poster">
@@ -25,12 +25,19 @@ fetch(json_url).then(Response => Response.json())
                         <div class="cont">
                             <h4>${name}</h4>
                         <div class="sub">
-                            <p>Action, 2022</p>
+                            <p>${genre}, ${date}</p>
                             <h3><span>IMDB</span><i class="bi bi-star-fill"></i>${imdb}</h3>
                         </div>
                     </div>
                 </div>
             `
-            
+            cards.appendChild(card);
+
         });
+
+        document.getElementById('title').innerText = date[0].name;
+        document.getElementById('gen').innerText = date[0].name;
+        document.getElementById('date').innerText = date[0].name;
+        document.getElementById('rate').innerHTML = `<span>IMDB</span><i class="bi bi-star-fill"></i> ${date[0].imdb}`;
+
     });
